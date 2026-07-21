99cbabbcb12d2d1605d67b1948af547e22797d79
t3hkitty <123781437+t3hkitty@users.noreply.github.com>
2026-07-21 14:51:36 -0700

    Update novelupdates.js

--- novelupdates.js
+++ novelupdates.js
@@ -2,27 +2,16 @@ var NovelUpdates = {};
 
 NovelUpdates.id = 'novelupdates-webnovel';
 NovelUpdates.name = 'NovelUpdates Webnovels';
-NovelUpdates.version = '1.1.2';
+NovelUpdates.version = '1.1.3';
 NovelUpdates.icon = 'https://www.novelupdates.com/favicon.ico';
 NovelUpdates.baseUrl = 'https://www.novelupdates.com';
 NovelUpdates.contentType = 'books';
-NovelUpdates.isWebnovel = true;
-
-NovelUpdates.capabilities = {
-    search: true,
-    discover: false,
-    download: false,
-    resolve: false,
-    searchDownloads: false,
-    bookChapters: true,
-    manga: false
-};
 
 NovelUpdates.search = async function(query) {
     return {
         results: [
             {
-                name: 'Webnovel Result: ' + query,
+                title: 'Webnovel Result: ' + query,
                 url: 'https://www.novelupdates.com/?s=' + encodeURIComponent(query),
                 coverUrl: 'https://www.novelupdates.com/favicon.ico',
                 summary: 'Webnovel source entry.'
