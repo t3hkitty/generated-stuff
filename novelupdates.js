e3290aeaa93f7269cdc56238a302ad20abd25e52
t3hkitty <123781437+t3hkitty@users.noreply.github.com>
2026-07-21 14:50:00 -0700

    Update novelupdates.js

--- novelupdates.js
+++ novelupdates.js
@@ -1,14 +1,34 @@
-{
-  "name": "Custom Repositories",
-  "extensions": [
-    {
-      "id": "novelupdates-webnovel",
-      "name": "NovelUpdates Webnovels",
-      "version": "1.1.2",
-      "description": "NovelUpdates metadata and web fiction extension",
-      "contentType": "books",
-      "scriptUrl": "https://raw.githubusercontent.com/t3hkitty/generated-stuff/main/novelupdates.js",
-      "icon": "https://www.novelupdates.com/favicon.ico"
-    }
-  ]
-}
+var NovelUpdates = {};
+
+NovelUpdates.id = 'novelupdates-webnovel';
+NovelUpdates.name = 'NovelUpdates Webnovels';
+NovelUpdates.version = '1.1.2';
+NovelUpdates.icon = 'https://www.novelupdates.com/favicon.ico';
+NovelUpdates.baseUrl = 'https://www.novelupdates.com';
+NovelUpdates.contentType = 'books';
+NovelUpdates.isWebnovel = true;
+
+NovelUpdates.capabilities = {
+    search: true,
+    discover: false,
+    download: false,
+    resolve: false,
+    searchDownloads: false,
+    bookChapters: true,
+    manga: false
+};
+
+NovelUpdates.search = async function(query) {
+    return {
+        results: [
+            {
+                name: 'Webnovel Result: ' + query,
+                url: 'https://www.novelupdates.com/?s=' + encodeURIComponent(query),
+                coverUrl: 'https://www.novelupdates.com/favicon.ico',
+                summary: 'Webnovel source entry.'
+            }
+        ]
+    };
+};
+
+__cinderExport = NovelUpdates;
