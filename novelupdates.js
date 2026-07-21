561c980c04976682f01ff2dc73ed1b6ae5252f77
t3hkitty <123781437+t3hkitty@users.noreply.github.com>
2026-07-21 14:49:27 -0700

    Update novelupdates.js

--- novelupdates.js
+++ novelupdates.js
@@ -1,22 +1,14 @@
-var NovelUpdates = {};
-
-NovelUpdates.id = 'novelupdates-webnovel';
-NovelUpdates.name = 'NovelUpdates Webnovels';
-NovelUpdates.version = '1.1.1';
-NovelUpdates.icon = 'https://www.novelupdates.com/favicon.ico';
-NovelUpdates.baseUrl = 'https://www.novelupdates.com';
-
-NovelUpdates.search = async function(query) {
-    return {
-        results: [
-            {
-                name: 'Result for ' + query,
-                url: 'https://www.novelupdates.com',
-                coverUrl: 'https://www.novelupdates.com/favicon.ico',
-                summary: 'Webnovel source mock mapping.'
-            }
-        ]
-    };
-};
-
-__cinderExport = NovelUpdates;
+{
+  "name": "Custom Repositories",
+  "extensions": [
+    {
+      "id": "novelupdates-webnovel",
+      "name": "NovelUpdates Webnovels",
+      "version": "1.1.2",
+      "description": "NovelUpdates metadata and web fiction extension",
+      "contentType": "books",
+      "scriptUrl": "https://raw.githubusercontent.com/t3hkitty/generated-stuff/main/novelupdates.js",
+      "icon": "https://www.novelupdates.com/favicon.ico"
+    }
+  ]
+}
