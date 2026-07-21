00afe6bf2ccb6db6db675f499bb45667109ac349
t3hkitty <123781437+t3hkitty@users.noreply.github.com>
2026-07-21 15:47:01 -0700

    Update novelupdates.js

--- novelupdates.js
+++ novelupdates.js
@@ -1,54 +1,50 @@
-(function() {
-    var extension = {
-        id: 'novelupdates-webnovel',
-        name: 'NovelUpdates Webnovels',
-        version: '1.3.0',
-        icon: 'https://www.novelupdates.com/favicon.ico',
-        baseUrl: 'https://www.novelupdates.com',
-        contentType: 'webnovels',
-        
-        capabilities: {
-            search: true,
-            discover: false,
-            download: false,
-            resolve: false,
-            searchDownloads: false,
-            bookChapters: true,
-            manga: false
+function NovelUpdatesSource() {
+    this.id = 'novelupdates-webnovel';
+    this.name = 'NovelUpdates Webnovels';
+    this.version = '1.3.1';
+    this.icon = 'https://www.novelupdates.com/favicon.ico';
+    this.baseUrl = 'https://www.novelupdates.com';
+    this.contentType = 'webnovels';
+    
+    this.capabilities = {
+        search: true,
+        discover: false,
+        download: false,
+        resolve: false,
+        searchDownloads: false,
+        bookChapters: true,
+        manga: false
+    };
+}
+
+NovelUpdatesSource.prototype.search = async function(query) {
+    return [
+        {
+            id: 'corpse-collector',
+            name: 'Corpse Collector',
+            title: 'Corpse Collector',
+            url: 'https://www.novelupdates.com/series/corpse-collector/',
+            cover: 'https://www.novelupdates.com/favicon.ico',
+            summary: 'A chilling tale tracking the harvest of mortal remains.'
         }
-    };
+    ];
+};
 
-    extension.search = async function(query) {
-        return [
-            {
-                id: 'corpse-collector',
-                name: 'Corpse Collector',
-                title: 'Corpse Collector',
-                url: 'https://www.novelupdates.com/series/corpse-collector/',
-                cover: 'https://www.novelupdates.com/favicon.ico',
-                summary: 'A chilling tale tracking the harvest of mortal remains.'
-            }
-        ];
-    };
+NovelUpdatesSource.prototype.getBookChapters = async function(bookUrl) {
+    return [
+        {
+            id: 'ch-1',
+            name: 'Chapter 1: The Gathering',
+            url: 'https://www.novelupdates.com/series/corpse-collector/'
+        }
+    ];
+};
 
-    extension.getBookChapters = async function(bookUrl) {
-        return [
-            {
-                id: 'ch-1',
-                name: 'Chapter 1: The Gathering',
-                url: 'https://www.novelupdates.com/series/corpse-collector/'
-            }
-        ];
+NovelUpdatesSource.prototype.getBookChapter = async function(chapterUrl) {
+    return {
+        title: 'Chapter 1: The Gathering',
+        content: '<p>The frost clings to the valley floor as the harvest begins.</p>'
     };
+};
 
-    extension.getBookChapter = async function(chapterUrl) {
-        return {
-            title: 'Chapter 1: The Gathering',
-            content: '<p>The frost clings to the valley floor as the harvest begins.</p>'
-        };
-    };
-
-    if (typeof __cinderExport !== 'undefined') {
-        __cinderExport = extension;
-    }
-})();
+__cinderExport = new NovelUpdatesSource();
