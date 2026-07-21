63a07154d7aa127c1668a0c7c713fcd8a23e48ce
t3hkitty <123781437+t3hkitty@users.noreply.github.com>
2026-07-21 14:55:37 -0700

    Update novelupdates.js

--- novelupdates.js
+++ novelupdates.js
@@ -2,7 +2,7 @@ var NovelUpdates = {};
 
 NovelUpdates.id = 'novelupdates-webnovel';
 NovelUpdates.name = 'NovelUpdates Webnovels';
-NovelUpdates.version = '1.1.4';
+NovelUpdates.version = '1.1.5';
 NovelUpdates.icon = 'https://www.novelupdates.com/favicon.ico';
 NovelUpdates.baseUrl = 'https://www.novelupdates.com';
 NovelUpdates.contentType = 'books';
@@ -10,10 +10,10 @@ NovelUpdates.contentType = 'books';
 NovelUpdates.search = async function(query) {
     return [
         {
-            title: 'Webnovel Result: ' + query,
+            title: query,
             url: 'https://www.novelupdates.com/?s=' + encodeURIComponent(query),
             coverUrl: 'https://www.novelupdates.com/favicon.ico',
-            summary: 'Webnovel source entry.'
+            summary: 'Matched entry for ' + query
         }
     ];
 };
